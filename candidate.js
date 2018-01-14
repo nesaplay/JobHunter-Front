610e2884b1b350195b6d629cec2b46232eaa75b9
Nenad Radovanovic <nesaplay@gmail.com>
2018-01-14 13:19:29 +0100

    IIFE and base for modal

--- candidate.js
+++ candidate.js
@@ -1,54 +1,78 @@
-var BASE_URL = 'http://localhost:3333/api/reports'
-var meta = JSON.parse(localStorage.getItem('CANDIDATE_ID'))
-var homeButton = document.querySelector('.header-button')
-var profilePicture = document.querySelector('.section-about-picture')
-var cName = document.querySelector('.section-about-name')
-var dob = document.querySelector('.section-about-dob')
-var email = document.querySelector('.section-about-email')
-var education = document.querySelector('.section-about-education')
-var table = document.querySelector('.section-reports-table').getElementsByTagName('tbody')[0]
+(function() {
+    // variables init
+    var BASE_URL = 'http://localhost:3333/api/reports'
+    var VIEW_ICON =
+        'http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons/glossy-black-icons-people-things/062525-glossy-black-icon-people-things-eye4-sc48.png'
+    var meta = JSON.parse(localStorage.getItem('CANDIDATE_ID'))
+    var homeButton = document.querySelector('.header-button')
+    var profilePicture = document.querySelector('.section-about-picture')
+    var cName = document.querySelector('.section-about-name')
+    var dob = document.querySelector('.section-about-dob')
+    var email = document.querySelector('.section-about-email')
+    var education = document.querySelector('.section-about-education')
+    var table = document
+        .querySelector('.section-reports-table')
+        .getElementsByTagName('tbody')[0]
 
-// Fetching reports
-fetch(BASE_URL)
-    .then(function(response) {
-        return response.json()
+    // Fetching reports
+    fetch(BASE_URL)
+        .then(function(response) {
+            return response.json()
+        })
+        .then(function(data) {
+            data
+                .filter(function(report) {
+                    return report.candidateId === meta.id
+                })
+                .map(populateList)
+        })
+
+    // Populating reports table
+    function populateList(report) {
+        var newRow = table.insertRow(table.rows.length)
+
+        var companyCell = newRow.insertCell(0)
+        var companyText = document.createTextNode(report.companyName)
+        companyCell.appendChild(companyText)
+
+        var interviewCell = newRow.insertCell(1)
+        var interviewText = document.createTextNode(
+            new Date(report.interviewDate).toDateString()
+        )
+        interviewCell.appendChild(interviewText)
+
+        var statusCell = newRow.insertCell(2)
+        var statusText = document.createTextNode(report.status)
+        statusCell.appendChild(statusText)
+
+        var viewCell = newRow.insertCell(3)
+        var viewIcon = document.createElement('img')
+        viewIcon.setAttribute('src', VIEW_ICON)
+        viewIcon.setAttribute('class', 'table-view-icon')
+        viewCell.setAttribute('class', 'table-status-cell')
+        viewCell.setAttribute('data-id', report.id)
+        viewCell.appendChild(viewIcon)
+    }
+
+    // Appending candidate info
+    profilePicture.setAttribute('src', meta.avatar)
+    cName.textContent = meta.name
+    dob.textContent = new Date(meta.birthday).toDateString()
+    email.textContent = meta.email
+    education.textContent = meta.education
+
+    homeButton.addEventListener('click', function() {
+        location.assign('index.html')
     })
-    .then(function(data) {
-        data
-            .filter(function(report) {
-                return report.candidateId === meta.id
-            })
-            .map(populateList)
+
+    document.addEventListener('click', function(e) {
+        console.log(e.target)
+        if (e.target && e.target.className === 'table-status-cell') {
+            console.log('open view here')
+        }
     })
 
-// Populating reports table
-function populateList(report) {
-    console.log(report);
-    var newRow = table.insertRow(table.rows.length);
-
-    var companyCell = newRow.insertCell(0);
-    var companyText = document.createTextNode(report.companyName);
-    companyCell.appendChild(companyText)
-
-    var interviewCell = newRow.insertCell(1);
-    var interviewText = document.createTextNode(new Date(report.interviewDate).toDateString());
-    interviewCell.appendChild(interviewText)
-
-    var statusCell = newRow.insertCell(2);
-    var statusText = document.createTextNode(report.status);
-    statusCell.appendChild(statusText)
-
-}
-
-// Appending candidate info
-profilePicture.setAttribute('src', meta.avatar)
-cName.textContent = meta.name
-dob.textContent = new Date(meta.birthday).toDateString()
-email.textContent = meta.email
-education.textContent = meta.education
-
-
-
-homeButton.addEventListener('click', function() {
-    location.assign('index.html')
-})
+    this.Modal = function() {
+        console.log('hey');
+    }
+})()
--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-var BASE_URL = 'http://localhost:3333/api'
-var IMG_PLACEHOLDER =
-    'https://akommo.com/static/img/email/person_placeholder.png'
-var candidateList = document.querySelector('.section-content')
-var homeButton = document.querySelector('.header-button')
+(function() {
+    var BASE_URL = 'http://localhost:3333/api'
+    var IMG_PLACEHOLDER =
+        'https://akommo.com/static/img/email/person_placeholder.png'
+    var candidateList = document.querySelector('.section-content')
+    var homeButton = document.querySelector('.header-button')
 
-// fetching candidates data
-fetch(`${BASE_URL}/candidates`)
-    .then(function(response) {
-        return response.json()
-    })
-    .then(function(data) {
-        candidateList.innerHTML = data.map(populateList).join('')
-    })
+    // fetching candidates data
+    fetch(`${BASE_URL}/candidates`)
+        .then(function(response) {
+            return response.json()
+        })
+        .then(function(data) {
+            candidateList.innerHTML = data.map(populateList).join('')
+        })
 
-function populateList(person) {
-    person.avatar = person.avatar || IMG_PLACEHOLDER
-    var meta = JSON.stringify(person)
+    function populateList(person) {
+        person.avatar = person.avatar || IMG_PLACEHOLDER
+        var meta = JSON.stringify(person)
 
-    return `
+        return `
             <article>
                 <div class='section-content-item' data-meta='${meta}'>
                     <img src=${person.avatar} />  
@@ -26,18 +27,19 @@ function populateList(person) {
                 </div>
             </article>
         `
-}
-
-homeButton.addEventListener('click', function() {
-    location.assign('index.html')
-})
-
-document.addEventListener('click', function(e) {
-    if (e.target && e.target.className === 'section-content-item') {
-        // get meta from data attribute and store it in localStorrage
-        var meta = e.target.getAttribute('data-meta')
-        localStorage.setItem('CANDIDATE_ID', meta)
-        // redirect to candidate.html
-        location.assign('candidate.html')
     }
-})
+
+    homeButton.addEventListener('click', function() {
+        location.assign('index.html')
+    })
+
+    document.addEventListener('click', function(e) {
+        if (e.target && e.target.className === 'section-content-item') {
+            // get meta from data attribute and store it in localStorrage
+            var meta = e.target.getAttribute('data-meta')
+            localStorage.setItem('CANDIDATE_ID', meta)
+            // redirect to candidate.html
+            location.assign('candidate.html')
+        }
+    })
+})()
